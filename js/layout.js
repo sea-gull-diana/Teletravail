f1e72d21046b2c8f20bd93895cfe1358f10b4ad0
sea-gull-diana <67862638+sea-gull-diana@users.noreply.github.com>
2021-11-24 11:19:15 +0100

    Modifier contact.html, ajouter jquery.js, ajouter 2 images dans Images/, modification de layout.js ne marche pas encore

--- js/layout.js
+++ js/layout.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const inHead = `
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="js/jquery.js"></script>
   `
   const containHead = document.createRange().createContextualFragment(inHead);
   head.append(containHead);
